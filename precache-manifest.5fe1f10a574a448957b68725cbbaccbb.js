7f6fca5c625c55cc4b28e0016f486e913718399f
kcvo <943514589@qq.com>
2021-02-25 11:41:27 +0800

    deploy

---
+++ precache-manifest.5fe1f10a574a448957b68725cbbaccbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e03a43a137aaaba68b8",
+    "url": "/morney-website/css/app.d7c5e09d.css"
+  },
+  {
+    "revision": "cdde37c7bb244902c56d4199ecda7681",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "4e03a43a137aaaba68b8",
+    "url": "/morney-website/js/app.a1f1e9ba.js"
+  },
+  {
+    "revision": "3f076dad305aeb0b5d0a",
+    "url": "/morney-website/js/chunk-vendors.47459d99.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "24b5db339ef3bca7436a1424bde3eed7",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
